e844dfa94daaee9560350e1af1535a6c3b6a058d
John Mark Fuentes <jmcaluyafuentes@gmail.com>
2025-06-21 01:39:28 +1000

    feat(backend): connect to MongoDB Atlas and logs connection status

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import foodRoutes from './routes/foodRoutes'
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Kalinga Baby app.')
+});
+
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Backend server is running on port ${PORT}.`));
+  })
+  .catch((err) => console.error(err));
